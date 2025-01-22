16f41dbeee13f2b71a9a3e84116a041fd1d3b1d0
Emad Rahimi <121079771+ersync@users.noreply.github.com>
2025-01-22 08:07:53 +0330

    build: setup image optimization and organization

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,33 @@
+// vite.config.ts
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    ViteImageOptimizer({
+      test: /\.(jpe?g|png|gif|tiff|webp|svg)$/i,
+      includePublic: true,
+      logStats: true,
+      png: {
+        quality: 90
+      },
+      jpeg: {
+        quality: 90
+      },
+      jpg: {
+        quality: 90
+      },
+      webp: {
+        lossless: false,
+        quality: 90,
+        alphaQuality: 90,
+        force: true
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
